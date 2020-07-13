141ee17f6bc19a2d2b3e18caafde267d1b7cf3de
Jiawei <owojw95@gmail.com>
2020-07-13 09:27:52 -0500

    modified #66

--- Week_01/66.plus-one.java
+++ Week_01/66.plus-one.java
@@ -7,14 +7,16 @@
 // @lc code=start
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = 0; i < digits.length; i++) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
         }
-        return digits;
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return res;
     }
 }
 // @lc code=end
